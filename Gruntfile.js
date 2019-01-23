9fcdee0b30447e6a4ce986e697507b5d4676bc47
Leonardo Val <leonardo.val@creatartis.com>
2019-01-22 21:21:02 -0300

    Build setup

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+﻿/** Gruntfile for [creatartis-base](http://github.com/LeonardoVal/list-utils.js).
+*/
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+	});
+
+	require('creatartis-grunt').config(grunt, {
+		globalName: 'list_utils',
+		sourceNames: ['__prologue__',
+			'core', 
+			'empty', 'singletons', 'arrays', 'strings',
+			'builders',
+			'folds',
+			'__epilogue__'],
+		deps: [ ],
+		jshint: { loopfunc: true, boss: true, evil: true, proto: true },
+		karma: ['Firefox', 'Chrome'],
+		connect: {
+			console: 'tests/console.html'
+		}
+	});
+
+	grunt.registerTask('full-test', ['test', 'karma:test_chrome']);
+	grunt.registerTask('default', ['build']);
+};
---
+++ src/__prologue__.js
@@ -0,0 +1,10 @@
+﻿/** Package wrapper and layout.
+*/
+function __init__() { "use strict";
+// Library layout. /////////////////////////////////////////////////////////////////////////////////
+	var exports = {
+		__package__: 'list-utils',
+		__name__: 'list_utils',
+		__init__: __init__,
+		__dependencies__: [ ]
+	};
