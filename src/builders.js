3526c97473ce0599ffb15b02e3ffc28b5ccf98be
Leonardo Val <leonardo.val@creatartis.com>
2019-01-24 00:08:33 -0300

    Added `enumFromThenTo` + Also changed the other iterators to use a `done` flag.

--- src/builders.js
+++ src/builders.js
@@ -10,15 +10,16 @@ Iterable.rangeIterator = function rangeIterator(from, to, step) {
 		case 1: to = from; from = 0; step = 1; break;
 		case 2: step = 1; break;
 	}
+	var done = false;
 	return {
 		next: function next_rangeIterator() {
-			var done = from > to,
-				value = from;
+			done = done && from > to;
+			var value = from;
 			from += step;
 			return done ? { done: true } : { value: value };
 		},
 		return: function return_rangeIterator() {
-			from = to + 1;
+			done = true;
 			return { done: true };
 		}
 	};
@@ -28,35 +29,61 @@ Iterable.range = function range(from, to, step) {
 	return new Iterable(Iterable.rangeIterator, from, to, step);
 };
 
-/*FIXME
+/** `enumFromThenTo(from=0, then=from+1, to=Infinity)` builds an Iterable object that goes over 
+enumeration defined by the given values: `from` being the first one and `then` being the second one.
+For example, `enumFromThenTo(1,3,8)` represents the sequence `[1,3,5,7]`.
+
+The big difference with `range` is that the enumeration can go in either direction. For example,
+`enumFromThenTo(10,7,0)` represents the sequence `[10,7,4,1]`.
+*/
 Iterable.enumFromThenTo = function enumFromThenTo(from, then, to) {
-	return Iterable.range(from, to, then - from);
+	if (typeof from === 'undefined') {
+		from = 0
+	}
+	if (typeof then === 'undefined') {
+		then = from + 1;
+	}
+	if (typeof to === 'undefined') {
+		to = then > from ? +Infinity : -Infinity;
+	}
+	var step = then - from,
+		done = false;
+	return {
+		next: function next_enumFromThenToIterator() {
+			done = done && (step > 0 ? from > to : from < to);
+			var value = from;
+			from += step;
+			return done ? { done: true } : { value: value };
+		},
+		return: function return_enumFromThenToIterator() {
+			done = true;
+			return { done: true };
+		}
+	};
 };
 
 Iterable.enumFromThen = function enumFromThen(from, then) {
-	var to = from < then ? +Infinity : -Infinity,
-		step = from < then ? +1 : -1;
-	return Iterable.range(from, to, step);
+	return Iterable.enumFromThenTo(from, then, then > from ? +Infinity : -Infinity);
 };
 
 Iterable.enumFrom = function enumFrom(from) {
-	return Iterable.range(from, Infinity, 1);
-}; */
+	return Iterable.enumFromThenTo(from, from + 1, +Infinity);
+};
 
 /** `repeat(value, n=Infinity)` builds an iterable that repeats the given `value` `n` times (or
 forever by default).
 */
 Iterable.repeatIterator = function repeatIterator(value, n) {
 	n = isNaN(n) ? Infinity : +n;
-	var i = 0;
+	var i = 0,
+		done = false;
 	return {
 		next: function next_repeatIterator() {
-			var done = i >= n;
-			i++;
+			done = done && (i++) >= n;
 			return done ? { done: true } : { value: value };
 		},
 		return: function return_repeatIterator() {
-			i = n;
+			done = true;
 			return { done: true };
 		}
 	};
@@ -72,15 +99,20 @@ the value `x`, `n` times (or indefinitely by default).
 Iterable.iterateIterator = function iterateIterator(f, x, n) {
 	n = isNaN(n) ? Infinity : +n;
 	var i = 0;
+		done = false;
 	return {
 		next: function next_iterateIterator() {
-			i++;
-			value = x;
-			x = f(x);
-			return i > n ? { done: true } : { value: value };
+			done = done && (i++) >= n;
+			if (done) {
+				return { done: true };
+			} else {
+				var value = x;
+				x = f(x);
+				return { value: value };
+			}
 		},
 		return: function return_iterateIterator() {
-			i = n;
+			done = true;
 			return { done: true };
 		}
 	};
