fc91bcbf74a30ab7336c30386b521a1e4f5dbf33
Leonardo Val <leonardo.val@creatartis.com>
2019-01-25 16:24:14 -0300

    Added default `length` and `get` methods

--- src/core.js
+++ src/core.js
@@ -14,6 +14,10 @@ var Iterable = exports.Iterable = function Iterable(iteratorFunction) {
 	this.__iter__ = iteratorFunction;
 };
 
+function __iter__(iterable) {
+	return iterable[ITERATOR]();
+}
+
 /** 
 */
 Iterable.subclass = function subclass(constructor, members) {
@@ -40,12 +44,39 @@ Iterable.subclass = function subclass(constructor, members) {
 	return constructor;
 };
 
+/** 
+*/
+Iterable.prototype.length = function length() {
+	var result = 0;
+	this.forEach(function () {
+		result++;
+	});
+	return result;
+};
+
+/**
+*/
+Iterable.prototype.get = function get(index, defaultValue) {
+	var result = defaultValue;
+	index = +index;
+	if (!isNaN(index)) {
+		this.forEach(function (value, i, iter) {
+			if (i === index) {
+				result = value;
+				iter.return(); // Abort the iteration.
+			}
+		});
+	}
+	//TODO Raise exception is no `defaultValue` is given.
+	return result;
+};
+
 /** `forEach(doFunction, ifFunction)` applies `doFunction` to all elements complying with
 `ifFunction`, and returns the last result. If no `ifFunction` is given, it iterates through all
 the elements in the sequence. Both functions get the current value and position as arguments.
 */
 Iterable.prototype.forEach = function forEach(doFunction, ifFunction) {
-	var iter = this[ITERATOR](), 
+	var iter = __iter__(this), 
 		i = 0,
 		current = iter.next(), 
 		result = current.value;
