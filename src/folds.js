b29cf9a060d424993b389b8547890893a680ee6d
Leonardo Val <leonardo.val@creatartis.com>
2019-01-28 22:16:43 -0300

    Implemented `scanl`.

--- src/folds.js
+++ src/folds.js
@@ -6,8 +6,7 @@ associative operator. The `initial` value is used as a starting point, but if it
 then the first element in the sequence is used.
 */
 Iterable.prototype.foldl = function foldl(foldFunction, initial) {
-	var iter = this.__iter__(), 
-		x;
+	var iter = __iter__(this);
 	if (typeof initial === 'undefined') {
 		initial = iter.next().value;
 	}
@@ -22,48 +21,52 @@ Iterable.prototype.foldl = function foldl(foldFunction, initial) {
 /** `scanl(foldFunction, initial)` folds the elements of this iterable with `foldFunction` as a left
 associative operator. Instead of returning the last result, it iterates over the intermediate values
 in the folding sequence.
-* /
-scanl: function scanl(foldFunction, initial) {
-	var from = this; // for closures.
-	return new Iterable(function __iter__() {
-		var iter = from.__iter__(), value, count = -1;
-		return function __scanlIterator__() {
-			count++;
-			if (count === 0) {
-				value = initial === undefined ? iter() : initial;
-			} else {
-				value = foldFunction(value, iter());
+*/
+Iterable.scanlIterator = function scanlIterator(list, foldFunction, initial) {
+	var iter = __iter__(list),
+		count = -1,
+		value = initial,
+		hasInitial = typeof value === 'undefined',
+		done = false;
+	return {
+		next: function next_scanlIterator() {
+			if (done) {
+				return { done: true };
 			}
-			return value;
-		};
-	});
-},
+			count++;
+			var x;
+			if (count === 0) {
+				if (!hasInitial) {
+					x = iter.next();
+					done = x.done;
+					if (done) {
+						return { done: true };
+					} else {
+						value = x.value;
+					}
+				}
+				return { value: value };
+			} else {
+				x = iter.next();
+				done = x.done;
+				if (done) {
+					return { done: true };
+				} else {
+					value = foldFunction(value, x.value);	
+					return { value: value };
+				}
+			}
+		},
+		return: function return_scanlIterator() {
+			done = true;
+			return { done: true };
+		}
+	};
+};
 
-/** `foldr(foldFunction, initial)` folds the elements of this iterable with `foldFunction` as a
-right associative operator. The `initial` value is used as a starting point, but if it is not
-defined the first element in the sequence is used.
-
-Warning! This is the same as doing a `foldl` in a reversed iterable.
-* /
-foldr: function foldr(foldFunction, initial) {
-	function flippedFoldFunction(x,y) {
-		return foldFunction(y,x);
-	}
-	return this.reverse().foldl(flippedFoldFunction, initial);
-},
-
-/** `scanr(foldFunction, initial)` folds the elements of this iterable with `foldFunction` as a
-right associative operator. Instead of returning the last result, it iterates over the
-intermediate values in the folding sequence.
-
-Warning! This is the same as doing a `scanl` in a reversed iterable.
-* /
-scanr: function scanr(foldFunction, initial) {
-	function flippedFoldFunction(x,y) {
-		return foldFunction(y,x);
-	}
-	return this.reverse().scanl(flippedFoldFunction, initial);
-},
+Iterable.prototype.scanl = function scanl(foldFunction, initial) {
+	return new Iterable(Iterable.scanlIterator, this, foldFunction, initial);
+};
 
 /** `sum(n=0)` returns the sum of all numbers in the sequence, or `n` if the sequence is empty.
 */
@@ -108,7 +111,7 @@ returns true, or if they are all truthy if no predicate is given. If the sequenc
 returned.
 */
 Iterable.prototype.all = function all(predicate, strict) {
-	predicate = typeof predicate === 'function' ? predicate : function (x) { return !!x; };
+	predicate = typeof predicate === 'function' ? predicate : __toBool__;
 	var result = true;
 	this.forEach(function (x, i, iter) {
 		if (!predicate(x, i)) {
@@ -125,7 +128,7 @@ Iterable.prototype.all = function all(predicate, strict) {
 returns false, or if the sequence is empty.
 */
 Iterable.prototype.any = function any(predicate, strict) {
-	predicate = typeof predicate === 'function' ? predicate : function (x) { return !!x; };
+	predicate = typeof predicate === 'function' ? predicate : __toBool__;
 	var result = false;
 	this.forEach(function (x, i, iter) {
 		if (predicate(x, i)) {
