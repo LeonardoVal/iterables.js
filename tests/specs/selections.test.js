1ec8e5e3de988f843b93d2ed377abbbdfd8fb870
Leonardo Val <leonardo.val@creatartis.com>
2019-02-10 23:51:47 -0300

    More test cases

--- tests/specs/selections.test.js
+++ tests/specs/selections.test.js
@@ -11,6 +11,33 @@
 			expectIterator(filterIterator(fromArray([0, 1, 2])), [1, 2]);
 			expectIterator(filterIterator(fromArray(['', 'a', 'aa', 'aaa'])), ['a', 'aa', 'aaa']);
 			expectIterator(filterIterator(fromArray(['', 0, null, false])), []);
+			expectIterator(filterIterator(fromArray([0, 1, 2, 3, 4]), function (n) {
+					return n % 2;
+				}), [1, 3]);
+			expectIterator(filterIterator(fromArray([0, 1, 2, 3, 4]), function (n) {
+					return n > 2;
+				}), [3, 4]);
+			expectIterator(filterIterator(fromArray(['', 'a', 'aa', 'aaa']), function (x) {
+					return x.length % 2;
+				}), ['a', 'aaa']);
+		});
+
+		it("`Iterable.filter` function", function () {
+			expect(list_utils.Iterable.prototype.filter).toBeOfType('function');
+			var fromArray = list_utils.Iterable.fromArray.bind(list_utils.Iterable);
+			expectList(fromArray([false, true, false]).filter(), [true]);
+			expectList(fromArray([0, 1, 2]).filter(), [1, 2]);
+			expectList(fromArray(['', 'a', 'aa', 'aaa']).filter(), ['a', 'aa', 'aaa']);
+			expectList(fromArray(['', 0, null, false]).filter(), []);
+			expectList(fromArray([0, 1, 2, 3, 4]).filter(function (n) {
+					return n % 2;
+				}), [1, 3]);
+			expectList(fromArray([0, 1, 2, 3, 4]).filter(function (n) {
+					return n > 2;
+				}), [3, 4]);
+			expectList(fromArray(['', 'a', 'aa', 'aaa']).filter(function (x) {
+					return x.length % 2;
+				}), ['a', 'aaa']);
 		});
 	}); // describe "Lists from arrays:"
 }); //// define
