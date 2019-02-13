c31d6ca3543b6cf9b32e0636c80d680a9397c9ab
LeonardoVal <leonardo.val@creatartis.com>
2019-02-12 21:39:46 -0300

    Test cases for functions related to indices + Some functions are still untested.

---
+++ tests/specs/indices.test.js
@@ -0,0 +1,32 @@
+﻿define(['list-utils', 'tests-common'], function (list_utils, test_common) { "use strict";
+	var expectIterator = test_common.expectIterator,
+		expectList = test_common.expectList;
+
+	describe("Lists indices:", function () {
+		it("`Iterable.indexOf` function", function () {
+			var array = [7, 'a', false, null],
+				arrayIterable = list_utils.Iterable.fromArray(array);
+			array.forEach(function (value, index) {
+				expect(arrayIterable.indexOf(value)).toBe(index);
+				expect(arrayIterable.indexOf(value, index + 1)).toBeLessThan(0);
+			});
+			expect(arrayIterable.indexOf(33)).toBeLessThan(0);
+		});
+
+		it("`Iterable.indexWhere` function", function () {
+			var array = [7, 'a', false, null],
+				arrayIterable = list_utils.Iterable.fromArray(array);
+			array.forEach(function (value, index) {
+				expect(arrayIterable.indexWhere(function (v) {
+					return typeof v === typeof value;
+				})).toBe(index);
+				expect(arrayIterable.indexWhere(function (v) {
+					return typeof v === typeof value;
+				}, index + 1)).toBeLessThan(0);
+			});
+			expect(arrayIterable.indexWhere(function () {
+				return false;
+			})).toBeLessThan(0);	
+		});
+	}); // describe "Lists indices:"
+}); //// define
