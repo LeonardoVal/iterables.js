441d2df4449dc8ab195558ba85beba8bea946a84
Leonardo Val <leonardo.val@creatartis.com>
2019-02-14 23:15:29 -0300

    Test cases for `zip`

---
+++ tests/specs/tuples.test.js
@@ -0,0 +1,33 @@
+﻿define(['list-utils', 'tests-common'], function (list_utils, test_common) { "use strict";
+	var expectIterator = test_common.expectIterator,
+		expectList = test_common.expectList;
+
+	describe("Lists of tuples:", function () {
+		it("zip()", function () {
+			var Iterable = list_utils.Iterable;
+			expect(Iterable.prototype.zip).toBeOfType('function');
+			expect(Iterable.zip).toBeOfType('function');
+			var fromString = Iterable.fromString.bind(Iterable);
+			[{ 
+				lists: [fromString('abc'), fromString('xyz')], 
+				expectedResult: [['a','x'], ['b','y'], ['c','z']]
+			 }, {
+				lists: [fromString('abc'), fromString('x')], 
+				expectedResult: [['a','x']]
+			 }, {
+				lists: [fromString('a'), fromString('xyz')], 
+				expectedResult: [['a','x']]
+			 }, {
+				lists: [fromString(''), fromString('xyz')], 
+				expectedResult: []
+			 }, {
+				lists: [fromString('abc'), fromString('')], 
+				expectedResult: []
+			 }].forEach(function (test) {
+				var lists = test.lists;
+				expectList(Iterable.prototype.zip.apply(lists[0], lists.slice(1)), test.expectedResult);
+				expectList(Iterable.zip.apply(Iterable, lists), test.expectedResult); 
+			});
+		});
+	}); // describe "Empty lists:"
+}); //// define
