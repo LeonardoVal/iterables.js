3f47bfa3c1816be742613e9ba1c50faa4b6c756e
Leonardo Val <leonardo.val@creatartis.com>
2019-02-22 12:50:50 -0300

    Added tests for `zipWith`

--- tests/specs/tuples.test.js
+++ tests/specs/tuples.test.js
@@ -30,6 +30,38 @@
 			});
 		});
 
+		it("zipWith()", function () {
+			var Iterable = list_utils.Iterable;
+			expect(Iterable.prototype.zipWith).toBeOfType('function');
+			expect(Iterable.zipWith).toBeOfType('function');
+			var fromString = Iterable.fromString.bind(Iterable),
+				fromValues = Iterable.fromValues.bind(Iterable);
+			[{	
+				func: function (vs) { return vs[0] + vs[1]; },
+				lists: [fromString('abc'), fromString('xyz')], 
+				expectedResult: ['ax', 'by', 'cz']
+			 }, {
+				func: function (vs) { return (+vs[0]) + (+vs[1]); },
+				lists: [fromString('123'), fromString('77')], 
+				expectedResult: [8, 9]
+			 }, {
+				func: function (vs) { return (+vs[0]) + (+vs[1]); },
+				lists: [fromString('123'), fromString('')], 
+				expectedResult: []
+			 }, {
+				func: function (vs) { return [vs[2], vs[0], vs[1]]; },
+				lists: [fromValues(1,2,3), fromString('abc'), fromValues(true,false,null)],
+				expectedResult: [[true,1,'a'], [false,2,'b'], [null,3,'c']]
+			 }].forEach(function (test) {
+				var func = test.func,
+					lists = test.lists;
+				expectList(Iterable.prototype.zipWith.apply(lists[0], [func].concat(lists.slice(1))),
+					test.expectedResult);
+				expectList(Iterable.zipWith.apply(Iterable, [func].concat(lists)), 
+					test.expectedResult); 
+			});
+		});
+
 		it("product()", function () {
 			var Iterable = list_utils.Iterable;
 			expect(Iterable.prototype.product).toBeOfType('function');
