09c5284913d08c0b21fc2c3c81c5d6f3a058c5c3
Leonardo Val <leonardo.val@creatartis.com>
2019-03-06 14:10:28 -0300

    Selection functions redefined using `filteredMapIterator`.

--- src/selections.js
+++ src/selections.js
@@ -4,103 +4,38 @@
 /** `filter(filterFunction)` returns an iterable of this iterable elements for which `condition`
 returns true.
 */
-Iterable.filterIterator = function filterIterator(list, condition) {
-	var iter = __iter__(list),
-		i = -1,
-		done = false;
-	condition = condition || __toBool__;
-	return {
-		next: function next_filterIterator() {
-			if (!done) do {
-				i++;
-				var x = iter.next();
-				done = x.done;
-				if (!done && condition(x.value, i, iter)) {
-					return { value: x.value };
-				}
-			} while (!done);
-			return { done: true };
-		},
-		return: function return_filterIterator() {
-			done = true;
-			return { done: true };
-		}
-	};
-};
-
-Iterable.prototype.filter = function filter(condition) {
-	return new Iterable(Iterable.filterIterator, this, condition);
-};
-
-/** 
-*/
-Iterable.filterMapIterator = function filterMapIterator(list, condition, mapFunction) {
-	var iter = __iter__(list),
-		i = -1,
-		done = false;
-	condition = condition || __toBool__;
-	return {
-		next: function next_filterMapIterator() {
-			if (!done) do {
-				i++;
-				var x = iter.next();
-				done = x.done;
-				if (!done && condition(x.value, i, iter)) {
-					return { value: mapFunction(x.value, i, iter) };
-				}
-			} while (!done);
-			return { done: true };
-		},
-		return: function return_filterMapIterator() {
-			done = true;
-			return { done: true };
-		}
-	};
-};
-
-/**
-*/
-Iterable.takeWhileIterator = function takeWhileIterator(list, condition) {
-	var iter = __iter__(list),
-		i = -1,
-		done = false;
-	condition = condition || __toBool__;
-	return {
-		next: function next_takeWhileIterator() {
-			var x; 
-			if (!done) {
-				i++;
-				x = iter.next();
-				done = x.done || !condition(x.value, i, iter);
-			}
-			if (!done) {
-				return { value: x.value };
-			} else {
-				return { done: true };
-			}
-		},
-		return: function return_takeWhileIterator() {
-			done = true;
-			return { done: true };
-		}
-	};
-};
+$iterationMethods(function filterIterator(list, condition) {
+	return filteredMapIterator(list, null, condition || __toBool__);
+});
 
 /** `takeWhile(condition)` return an iterable with the first elements that verify the given
 `condition`.
 */
-Iterable.prototype.takeWhile = function takeWhile(condition) {
-	return new Iterable(Iterable.takeWhileIterator, this, condition);
-};
+$iterationMethods(function takeWhileIterator(list, condition) {
+	condition = condition || __toBool__;
+	return filteredMapIterator(list, null, function (value, i, iter) {
+		if (!condition(value, i, iter)) {
+			iter.return();
+			return false;
+		} else {
+			return true;
+		}
+	});
+});
 
 /** `take(n=1)` return an iterable with the first `n` elements of this one.
 */
-Iterable.prototype.take = function take(n) {
-	n = isNaN(n) ? 1 : n | 0;
-	return new Iterable(Iterable.takeWhileIterator, this, function condition(_, i) {
-		return i < n;
+$iterationMethods(function takeIterator(list, n) {
+	n = isNaN(n) ? 1 : Math.floor(n);
+	return filteredMapIterator(list, null, function (value, i, iter) {
+		if (i >= n) {
+			iter.return();
+			return false;
+		} else {
+			return true;
+		}
 	});
-};
+});
 
 /** `head(defaultValue)` returns the first element. If the sequence is empty it returns 
 `defaultValue`, or raise an exception if none is given.
@@ -121,49 +56,23 @@ Iterable.prototype.head = function head(defaultValue) {
 /** `dropWhile(condition)` returns an iterable with the same elements than this, except the
 first ones that comply with the condition.
 */
-Iterable.dropWhileIterator = function dropWhileIterator(list, condition) {
-	var iter = __iter__(list),
-		i = -1,
-		done = false,
-		finishedDroping = false;
+$iterationMethods(function dropWhileIterator(list, condition) {
 	condition = condition || __toBool__;
-	return {
-		next: function next_dropWhileIterator() {
-			var x;
-			if (!finishedDroping) {
-				while (!done) {
-					i++;
-					x = iter.next();
-					done = x.done;
-					if (!done && !condition(x.value, i, iter)) {
-						break;
-					}
-				}
-				finishedDroping = true;
-				return done ? { done: true } : { value: x.value };
-			} else {
-				return done ? { done: true } : iter.next();
-			}
-		},
-		return: function return_dropWhileIterator() {
-			done = true;
-			return { done: true };
-		}
-	};
-};
-
-Iterable.prototype.dropWhile = function dropWhile(condition) {
-	return new Iterable(Iterable.dropWhileIterator, this, condition);
-};
+	var dropping = true;
+	return filteredMapIterator(list, null, function (value, i, iter) {
+		dropping = dropping && condition(value, i, iter);
+		return !dropping;
+	});
+});
 
 /** `drop(n=1)` returns an iterable with the same elements than this, except the first `n` ones.
 */
-Iterable.prototype.drop = function drop(n) {
-	n = isNaN(n) ? 1 : n | 0;
-	return new Iterable(Iterable.dropWhileIterator, this, function condition(_, i) {
-		return i < n;
+$iterationMethods(function dropIterator(list, n) {
+	n = isNaN(n) ? 1 : Math.floor(n);
+	return filteredMapIterator(list, null, function (value, i, iter) {
+		return i >= n;
 	});
-};
+});
 
 /** `tail()` returns an iterable with the same elements than this, except the first one.
 */
