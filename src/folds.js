cbd7ef7dd6ba30cac65c2305e188bba7b736bba3
Leonardo Val <leonardo.val@creatartis.com>
2019-03-06 14:04:00 -0300

    Folding functions redefined using `filteredMapIterator` and `lastFromIterator`.

--- src/folds.js
+++ src/folds.js
@@ -1,109 +1,53 @@
 /**
 */
 
+/** `scanl(foldFunction, initial)` folds the elements of this iterable with `foldFunction` as a left
+associative operator. Instead of returning the last result, it iterates over the intermediate values
+in the folding sequence.
+*/
+$iterationMethods(function scanlIterator(list, foldFunction, initial) {
+	var folded = initial;
+	return filteredMapIterator(list, function (value, i, iter) {
+		folded = foldFunction(folded, value, i, iter);
+		return folded;
+	});
+});
+
 /** `foldl(foldFunction, initial)` folds the elements of this iterable with `foldFunction` as a left
 associative operator. The `initial` value is used as a starting point, but if it is not defined, 
 then the first element in the sequence is used.
 */
 Iterable.prototype.foldl = function foldl(foldFunction, initial) {
-	var iter = __iter__(this);
-	if (typeof initial === 'undefined') {
-		initial = iter.next().value;
-	}
-	var x = iter.next();
-	for (var i = 0; !x.done; i++) {
-		initial = foldFunction(initial, x.value, i, iter);
-		x = iter.next();
-	}
-	return initial;
-};
-
-/** `scanl(foldFunction, initial)` folds the elements of this iterable with `foldFunction` as a left
-associative operator. Instead of returning the last result, it iterates over the intermediate values
-in the folding sequence.
-*/
-Iterable.scanlIterator = function scanlIterator(list, foldFunction, initial) {
-	var iter = __iter__(list),
-		count = -1,
-		value = initial,
-		hasInitial = typeof value === 'undefined',
-		done = false;
-	return {
-		next: function next_scanlIterator() {
-			if (done) {
-				return { done: true };
-			}
-			count++;
-			var x;
-			if (count === 0) {
-				if (!hasInitial) {
-					x = iter.next();
-					done = x.done;
-					if (done) {
-						return { done: true };
-					} else {
-						value = x.value;
-					}
-				}
-				return { value: value };
-			} else {
-				x = iter.next();
-				done = x.done;
-				if (done) {
-					return { done: true };
-				} else {
-					value = foldFunction(value, x.value);	
-					return { value: value };
-				}
-			}
-		},
-		return: function return_scanlIterator() {
-			done = true;
-			return { done: true };
-		}
-	};
-};
-
-Iterable.prototype.scanl = function scanl(foldFunction, initial) {
-	return new Iterable(Iterable.scanlIterator, this, foldFunction, initial);
+	return lastFromIterator(Iterable.scanlIterator(this, foldFunction, initial), initial);
 };
 
 /** `sum(n=0)` returns the sum of all numbers in the sequence, or `n` if the sequence is empty.
 */
 Iterable.prototype.sum = function sum(n) {
-	var result = isNaN(n) ? 0 : +n;
-	this.forEach(function (x) {
-		result += +x;
-	});
-	return result;
+	n = isNaN(n) ? 0 : +n;
+	return this.foldl(function (n1, n2) {
+		return n1 + n2;
+	}, n);
 };
 
 /** `min(n=Infinity)` returns the minimum of all numbers in the sequence, or `Infinity` if the 
 sequence is empty.
 */
 Iterable.prototype.min = function min(n) {
-	var result = isNaN(n) ? Infinity : +n;
-	this.forEach(function (x) {
-		x = +x;
-		if (x < result) {
-			result = x;
-		}
-	});
-	return result;
+	n = isNaN(n) ? Infinity : +n;
+	return this.foldl(function (n1, n2) {
+		return Math.min(n1, n2);
+	}, n);
 };
 
 /** `max(n=-Infinity)` returns the maximum of all numbers in the sequence, or `-Infinity` if the 
 sequence is empty.
 */
 Iterable.prototype.max = function max(n) {
-	var result = isNaN(n) ? -Infinity : +n;
-	this.forEach(function (x) {
-		x = (+x);
-		if (x > result) {
-			result = x;
-		}
-	});
-	return result;
+	n = isNaN(n) ? Infinity : +n;
+	return this.foldl(function (n1, n2) {
+		return Math.max(n1, n2);
+	}, n);
 };
 
 /** `all(predicate, strict=false)` returns true if for all elements in the sequence `predicate`
@@ -112,16 +56,15 @@ returned.
 */
 Iterable.prototype.all = function all(predicate, strict) {
 	predicate = typeof predicate === 'function' ? predicate : __toBool__;
-	var result = true;
-	this.forEach(function (x, i, iter) {
-		if (!predicate(x, i, iter)) {
+	return this.foldl(function (result, value, i, iter) {
+		if (!predicate(value, i, iter)) {
 			result = false;
 			if (!strict) {
 				iter.return(); // Shortcircuit.
 			}
 		}
-	});
-	return result;
+		return result;
+	}, true);
 };
 
 /** `any(predicate, strict=false)` returns false if for all elements in the sequence `predicate`
@@ -129,14 +72,13 @@ returns false, or if the sequence is empty.
 */
 Iterable.prototype.any = function any(predicate, strict) {
 	predicate = typeof predicate === 'function' ? predicate : __toBool__;
-	var result = false;
-	this.forEach(function (x, i, iter) {
-		if (predicate(x, i, iter)) {
+	return this.foldl(function (result, value, i, iter) {
+		if (predicate(value, i, iter)) {
 			result = true;
 			if (!strict) {
 				iter.return(); // Shortcircuit.
 			}
 		}
-	});
-	return result;
+		return result;
+	}, false);
 };
