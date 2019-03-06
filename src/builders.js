f02adeadc294377dbb597357e454cc6f4efd8be6
Leonardo Val <leonardo.val@creatartis.com>
2019-03-06 14:02:20 -0300

    Sequence builders redefined using `generatorIterator` et al.

--- src/builders.js
+++ src/builders.js
@@ -4,36 +4,29 @@
 /** `range(from=0, to, step=1)` builds an Iterable object with number from `from` upto `to` with
 the given `step`. For example, `range(2,12,3)` represents the sequence `[2, 5, 8, 11]`.
 */
-Iterable.rangeIterator = function rangeIterator(from, to, step) {
-	switch (arguments.length) {
-		case 0: return rangeIterator(0, 0, 1);
-		case 1: return rangeIterator(0, from, 1);
-		case 2: return rangeIterator(from, to, 1);
-		//default: continue;
+$builderMethods(function rangeIterator(from, to, step) {
+	if (typeof from === 'undefined') {
+		from = 0;
 	}
-	var done = false;
-	return {
-		next: function next_rangeIterator() {
-			done = done || from >= to;
-			var value = from;
+	if (typeof to === 'undefined') {
+		to = from;
+		from = 0;
+	}
+	if (typeof step === 'undefined') {
+		step = 1;
+	}
+	from = +from;
+	to = +to;
+	step = +step;
+	return generatorIterator(function (obj) {
+		if (from >= to) {
+			obj.done = true;
+		} else {
+			obj.value = from;
 			from += step;
-			return done ? { done: true } : { value: value };
-		},
-		return: function return_rangeIterator() {
-			done = true;
-			return { done: true };
 		}
-	};
-};
-
-Iterable.range = function range(from, to, step) {
-	switch (arguments.length) {
-		case 0: return new Iterable(Iterable.rangeIterator, 0, 0, 1);
-		case 1: return new Iterable(Iterable.rangeIterator, 0, from, 1);
-		case 2: return new Iterable(Iterable.rangeIterator, from, to, 1);
-		default: new Iterable(Iterable.rangeIterator, from, to, step);
-	}
-};
+	});
+});
 
 /** `enumFromThenTo(from=0, then=from+1, to=Infinity)` builds an Iterable object that goes over 
 enumeration defined by the given values: `from` being the first one and `then` being the second one.
@@ -52,20 +45,15 @@ Iterable.enumFromThenToIterator = function enumFromThenToIterator(from, then, to
 	if (typeof to === 'undefined') {
 		to = then > from ? +Infinity : -Infinity;
 	}
-	var step = then - from,
-		done = false;
-	return {
-		next: function next_enumFromThenToIterator() {
-			done = done || (step > 0 ? from > to : from < to);
-			var value = from;
+	var step = then - from;
+	return generatorIterator(function (obj) {
+		if (step > 0 ? from > to : from < to) {
+			obj.done = true;
+		} else {
+			obj.value = from;
 			from += step;
-			return done ? { done: true } : { value: value };
-		},
-		return: function return_enumFromThenToIterator() {
-			done = true;
-			return { done: true };
 		}
-	};
+	});
 };
 
 Iterable.enumFromThenTo = function enumFromThenTo(from, then, to) {
@@ -83,51 +71,28 @@ Iterable.enumFrom = function enumFrom(from) {
 /** `repeat(value, n=Infinity)` builds an iterable that repeats the given `value` `n` times (or
 forever by default).
 */
-Iterable.repeatIterator = function repeatIterator(value, n) {
+$builderMethods(function repeatIterator(value, n) {
 	n = isNaN(n) ? Infinity : +n;
-	var i = 0,
-		done = false;
-	return {
-		next: function next_repeatIterator() {
-			done = done || (i++) >= n;
-			return done ? { done: true } : { value: value };
-		},
-		return: function return_repeatIterator() {
-			done = true;
-			return { done: true };
+	return generatorWithIndexIterator(function (obj, i) {
+		if (i >= n) {
+			obj.done = true;
+		} else {
+			obj.value = value;
 		}
-	};
-};
-
-Iterable.repeat = function repeat(value, n) {
-	return new Iterable(Iterable.repeatIterator, value, n);
-};
+	});
+});
 
 /** `iterate(f, x, n=Infinity)` returns an iterable that repeatedly applies the function `f` to
 the value `x`, `n` times (or indefinitely by default).
 */
-Iterable.iterateIterator = function iterateIterator(f, x, n) {
+$builderMethods(function iterateIterator(f, x, n) {
 	n = isNaN(n) ? Infinity : +n;
-	var i = 0;
-		done = false;
-	return {
-		next: function next_iterateIterator() {
-			done = done || (i++) >= n;
-			if (done) {
-				return { done: true };
-			} else {
-				var value = x;
-				x = f(x);
-				return { value: value };
-			}
-		},
-		return: function return_iterateIterator() {
-			done = true;
-			return { done: true };
+	return generatorWithIndexIterator(function (obj, i) {
+		if (i >= n) {
+			obj.done = true;
+		} else {
+			obj.value = x;
+			x = f(x);
 		}
-	};
-};
-
-Iterable.iterate = function iterate(f, x, n) {
-	return new Iterable(Iterable.iterateIterator, f, x, n);
-};
+	});
+});
