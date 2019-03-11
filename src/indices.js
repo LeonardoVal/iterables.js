868edb95fe0c48d1aa59f49ba9151857e02d588f
Leonardo Val <leonardo.val@creatartis.com>
2019-03-10 23:56:58 -0300

    Redefined indices functions using `filteredMapIterator`.

--- src/indices.js
+++ src/indices.js
@@ -1,58 +1,33 @@
 /**
 */
 
+/** `indicesWhere(condition, from=0)` is a sequence of the positions in this iterable of values
+that comply with the given `condition`.
+*/
+$methodOn1List(function indicesWhereIterator(list, condition, from) {
+	from = Math.floor(from) || 0;
+	return filteredMapIterator(list, 
+		function (_, i) {
+			return i;
+		},
+		function (v, i, iter) { 
+			return condition(v, i, iter) && i >= from; 
+		}
+	);
+});
+
 /** `indexWhere(condition, from=0)` returns the position of the first value of this iterable
 that complies with the given `condition`, or -1 if there is none.
 */
 Iterable.prototype.indexWhere = function indexWhere(condition, from) {
-	var iter = __iter__(this),
-		x = iter.next();
-	from = from |0;
-	for (var i = 0; !x.done; i++) {
-		if (i >= from && condition(x.value, i, iter)) {
-			return i;
-		}
-		x = iter.next();
-	}
-	return -1;
+	return this.indicesWhere(condition, from).head(-1);
 };
 
-/** `indexesWhere(condition, from=0)` is a sequence of the positions in this iterable of values
-that comply with the given `condition`.
+/** `indicesOf(value, from=0)` is a sequence of the positions of the value in this iterable.
 */
-Iterable.prototype.indicesWhere = function indexesWhere(condition, from) {
-	from = from|0;
-	return new Iterable(Iterable.filterMapIterator, this, 
-		function (v, i, iter) { 
-			return condition(v, i, iter) && i >= from; 
-		},
-		function (_, i) {
-			return i;
-		}
-	);
-};
-
-/** `indexOf(value, from=0)` is analogous to the array's namesake method. Returns the first
-	position of the given `value`, or -1 if it is not found.
-*/
-Iterable.prototype.indexOf = function indexOf(value, from) {
-	var iter = __iter__(this),
-		x = iter.next();
-	from = from |0;
-	for (var i = 0; !x.done; i++) {
-		if (x.value === value && i >= from) {
-			return i;
-		}
-		x = iter.next();
-	}
-	return -1;
-};
-
-/** `indexesOf(value, from=0)` is a sequence of the positions of the value in this iterable.
-*/
-Iterable.prototype.indicesOf = function indexesOf(value, from) {
-	from = from|0;
-	return new Iterable(Iterable.filteredMapIterator, this, 
+$methodOn1List(function indicesOfIterator(list, value, from) {
+	from = Math.floor(from) || 0;
+	return filteredMapIterator(list, 
 		function (_, i) {
 			return i;
 		},
@@ -60,4 +35,11 @@ Iterable.prototype.indicesOf = function indexesOf(value, from) {
 			return v === value && i >= from;
 		}
 	);
+});
+
+/** `indexOf(value, from=0)` is analogous to the array's namesake method. Returns the first
+	position of the given `value`, or -1 if it is not found.
+*/
+Iterable.prototype.indexOf = function indexOf(value, from) {
+	return this.indicesOf(value, from).head(-1);
 };
