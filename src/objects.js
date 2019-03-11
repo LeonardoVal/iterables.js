371cb7d213d4d7782209599a0f4e9639f661fb96
Leonardo Val <leonardo.val@creatartis.com>
2019-03-10 23:55:43 -0300

    `toObject` is now defined using `filteredMapIterator`.

--- src/objects.js
+++ src/objects.js
@@ -21,8 +21,8 @@ a new one by default) each key-value pairs as a property.
 */
 Iterable.prototype.toObject = function toObject(obj) {
 	obj = obj || {};
-	this.forEach(function (x) {
-		obj[x[0]] = x[1];
-	});
-	return obj;
+	return lastFromIterator(filteredMapIterator(this, function (pair) {
+		obj[pair[0]] = pair[1];
+		return obj;
+	}), obj);
 };
