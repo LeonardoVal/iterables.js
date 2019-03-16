0670f918c709f53eb4d444a8cd71c783662e4fa1
Leonardo Val <leonardo.val@creatartis.com>
2019-03-16 11:44:55 -0300

    Asynchronous iterators + Removed deprecated functions. + Changed `trickleIterator` to `ticksIterator`, implemented with `generatorIterator`.

--- src/async.js
+++ src/async.js
@@ -1,24 +1,13 @@
 /**
 */
-
-/**
-*/
 Iterable.prototype.isAsync = function isAsync() {
 	return !!this[ASYNC_ITERATOR];
 };
 
-/** 
-*/
-Iterable.asyncIteratorFunction = function asyncIteratorFunction(iteratorFunction) {
-	iteratorFunction[IS_ASYNC_ID] = true;
-	return iteratorFunction;
-};
-var asyncIteratorFunction = Iterable.asyncIteratorFunction;
-
 /** A `mockAsyncIterator` asynchronously iterates over a synchronous iterable. Useful mostly for
 testing purposes. 
 */
-Iterable.mockAsyncIterator = asyncIteratorFunction(function mockAsyncIterator(list) {
+Iterable.mockAsyncIterator = function mockAsyncIterator(list) {
 	var iter = __iter__(list, false),
 		done = false;
 	return generatorIterator(function (obj) {
@@ -30,36 +19,24 @@ Iterable.mockAsyncIterator = asyncIteratorFunction(function mockAsyncIterator(li
 		}
 		return Promise.resolve(obj);
 	});
-});
+};
+Iterable.mockAsyncIterator.isAsync = true;
 
 /**
-*/
-Iterable.trickleIterator = function trickleIterator(list, time) {
-	time = Math.floor(time) || 0;
-	var iter = __iter__(list),
-		done = false;
-	return {
-		next: function next_trickleIterator() {
-			var x;
-			if (!done) {
-				try {
-					x = iter.next();
-				} catch (err) {
-					return Promise.reject(err);
-				}
-				done = x.done;
-			}
-			if (done) {
-				return Promise.resolve({ done: true });
-			} else {
-				return new Promise(function executor(resolve, reject) { 
-					setTimeout(resolve.bind(null, x), time);
-				});
-			}
-		},
-		return: function return_trickleIterator() {
-			done = true;
-			return Promise.resolve({ done: true });
+ * 
+ */
+$builderMethod(function ticksIterator(step, end) {
+	return generatorIterator(function (obj) {
+		if (Date.now() >= end) {
+			obj.done = true;
+			return Promise.resolve(obj);
+		} else {
+			return new Promise(function executor(resolve, reject) { 
+				setTimeout(function () {
+					obj.value = Date.now();
+					resolve(obj);
+				}, step);
+			});
 		}
-	};
-};
+	});
+}, true);
