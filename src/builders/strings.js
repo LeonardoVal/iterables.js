e261e017e6fecbb6bc5dabea0e0307e8652ec3ee
Leonardo Val <leonardo.val@creatartis.com>
2019-03-17 23:10:45 -0300

    String iterable + `StringIterable` redefined on `$subtype` and moved to `builders/`. + `join` separated in its own script in `conversions/`.

---
+++ src/builders/strings.js
@@ -0,0 +1,20 @@
+/**
+ */
+var StringIterable = $subtype(function stringIterator(string) {
+	if (typeof string !== 'string') {
+		throw new TypeError('Argument must be a string, but is a `'+ typeof string +'`!');
+	}
+	return generatorWithIndexIterator(function (obj, i) {
+		if (i >= string.length) {
+			obj.done = true;
+		} else {
+			obj.value = string.charAt(i);
+		}
+	});
+});
+
+/** 
+ */
+Iterable.fromString = function fromString(string) {
+	return new exports.StringIterable(string);
+};
