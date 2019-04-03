07fc6b83861be9383505596e66f6ce457e862c9a
Leonardo Val <leonardo.val@creatartis.com>
2019-04-03 10:54:37 -0300

    Function `max` default value should be `-Infinity`.

--- src/reductions/max.js
+++ src/reductions/max.js
@@ -2,7 +2,7 @@
 sequence is empty.
 */
 Iterable.prototype.max = function max(n) {
-	n = isNaN(n) ? Infinity : +n;
+	n = isNaN(n) ? -Infinity : +n;
 	return this.foldl(function (n1, n2) {
 		return Math.max(n1, n2);
 	}, n);
