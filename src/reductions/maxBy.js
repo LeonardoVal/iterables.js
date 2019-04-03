c56e744f2301d828c1ad64aba92786f3d0d35fd2
Leonardo Val <leonardo.val@creatartis.com>
2019-04-03 10:56:31 -0300

    Added functions `minBy` and `maxBy` + These are `max` and `min` analogues, that use a `compareFunction`.

---
+++ src/reductions/maxBy.js
@@ -0,0 +1,8 @@
+/** `maxBy(compareFunction, defaultValue)` returns the maximum of all numbers in the sequence, 
+ * according to the given `compareFunction`, or `defaultValue` if the sequence is empty.
+ */
+Iterable.prototype.maxBy = function maxBy(compareFunction, defaultValue) {
+	return this.foldl(function (v1, v2) {
+		return compareFunction(v1, v2) < 0 ? v2 : v1;
+	}, defaultValue);
+};
