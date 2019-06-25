a2b857809360c062ff221acf9e7c38d2551d8f3a
LeonardoVal <leonardo.val@creatartis.com>
2019-06-25 15:22:29 -0300

    Generator `properties` + Used in `ObjectIterable`  to go through an `Object` properties. + Also updated some documentation.

--- src/generators.js
+++ src/generators.js
@@ -4,9 +4,9 @@ let generators = {
 
 // Sequence builders ///////////////////////////////////////////////////////////
 
-	/** `range(from=0, to, step=1)` builds an Iterable object with number from 
-	 * `from` upto `to` with the given `step`. For example, `range(2,12,3)` 
-	 * represents the sequence `[2, 5, 8, 11]`.
+	/** `range(from=0, to, step=1)` generates a sequence of numbers from `from`
+	 * upto `to` with the given `step`. For example, `range(2,12,3)` represents
+	 * the sequence `[2, 5, 8, 11]`.
 	 */
 	*range(from, to, step) {
 		if (typeof from === 'undefined') {
@@ -28,10 +28,10 @@ let generators = {
 		}
 	},
 
-	/** `enumFromThenTo(from=0, then=from+1, to=Infinity)` builds an Iterable 
-	 * object that goes over enumeration defined by the given values: `from` 
-	 * being the first one and `then` being the second one. For example, 
-	 * `enumFromThenTo(1,3,8)` represents the sequence `[1,3,5,7]`.
+	/** `enumFromThenTo(from=0, then=from+1, to=Infinity)` generates an
+	 * enumeration defined by the given values: `from` being the first one and 
+	 * `then` being the second one. For example, `enumFromThenTo(1,3,8)` 
+	 * represents the sequence `[1,3,5,7]`.
 	 * 
 	 * The big difference with `range` is that the enumeration can go in either
 	 * direction. For example, `enumFromThenTo(10,7,0)` represents the 
@@ -62,7 +62,7 @@ let generators = {
 		yield* this.enumFromThen(from, undefined);
 	},
 
-	/** `repeat(value, n=Infinity)` builds an iterable that repeats the given 
+	/** `repeat(value, n=Infinity)` generates a sequence that repeats the given 
 	 * `value` `n` times (or forever by default).
 	 */
 	*repeat(value, n) {
@@ -72,7 +72,7 @@ let generators = {
 		}
 	},
 
-	/** `iterate(f, arg, n=Infinity)` returns an iterable that repeatedly 
+	/** `iterate(f, arg, n=Infinity)` generates a sequence that repeatedly 
 	 * applies the function `f` to `arg`, `n` times (or indefinitely by 
 	 * default).
 	 */
@@ -82,6 +82,22 @@ let generators = {
 		for (let i = 0; i < n; i++) {
 			yield arg;
 			arg = f.call(null, arg, i);
+		}
+	},
+
+	/** `properties(obj, keys)` generates a sequence of `[key,value]` pairs,
+	 * with a key taken from `keys` and the value of the corresponding property 
+	 * of `obj`. If `keys` is not given, all of the `obj`'s keys are used. 
+	 */
+	*properties(obj, keys = null) {
+		if (keys) {
+			for (let key of keys) {
+				yield [key, obj[key]];
+			}
+		} else {
+			for (let key in obj) {
+				yield [key, obj[key]];
+			}
 		}
 	},
 
