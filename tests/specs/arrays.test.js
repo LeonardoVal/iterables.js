d568ce5db5b1611d625969c003f68d73b7c3395d
Leonardo Val <leonardo.val@creatartis.com>
2019-07-14 02:28:52 -0300

    Updated array tests + Removed test cases for iterators. + Cleaned up the code of the other tests.

--- tests/specs/arrays.test.js
+++ tests/specs/arrays.test.js
@@ -1,28 +1,20 @@
-﻿define(['list-utils', 'tests-common'], function (list_utils, test_common) { "use strict";
-	var expectIterator = test_common.expectIterator,
-		expectList = test_common.expectList;
+﻿define(['list-utils', 'tests-common'], function (listUtils, test_common) { "use strict";
+	var expectList = test_common.expectList;
 
 	describe("Lists from arrays:", function () {
-		it("`Iterable.arrayIterator` function", function () {
-			expect(list_utils.Iterable.fromArrayIterator).toBeOfType('function');
-			var arrayIterator = list_utils.Iterable.fromArrayIterator.bind(list_utils.Iterable);
-			expectIterator(arrayIterator([]), []);
-			expectIterator(arrayIterator([true]), [true]);
-			expectIterator(arrayIterator([1, 2]), [1, 2]);
-			expectIterator(arrayIterator(['a', 'b', 'c']), ['a', 'b', 'c']);
-		});
-
 		it("`ArrayIterable` class", function () {
-			expect(list_utils.FromArrayIterable).toBeOfType('function');
-			expectList(new list_utils.FromArrayIterable([]), []);
-			expectList(new list_utils.FromArrayIterable([true]), [true]);
-			expectList(new list_utils.FromArrayIterable([1, 2]), [1, 2]);
-			expectList(new list_utils.FromArrayIterable(['a', 'b', 'c']), ['a', 'b', 'c']);
+			let ArrayIterable = listUtils.ArrayIterable;
+			expect(ArrayIterable).toBeOfType('function');
+			expectList(new ArrayIterable([]), []);
+			expectList(new ArrayIterable([true]), [true]);
+			expectList(new ArrayIterable([1, 2]), [1, 2]);
+			expectList(new ArrayIterable(['a', 'b', 'c']), ['a', 'b', 'c']);
 		});
 
 		it("`Iterable.fromArray` function", function () {
-			expect(list_utils.Iterable.fromArray).toBeOfType('function');
-			var fromArray = list_utils.Iterable.fromArray.bind(list_utils.Iterable);
+			let Iterable = listUtils.Iterable;
+			expect(Iterable.fromArray).toBeOfType('function');
+			var fromArray = Iterable.fromArray.bind(Iterable);
 			expectList(fromArray([]), []);
 			expectList(fromArray([true]), [true]);
 			expectList(fromArray([1, 2]), [1, 2]);
@@ -30,8 +22,9 @@
 		});
 
 		it("`Iterable.fromValues` function", function () {
-			expect(list_utils.Iterable.fromValues).toBeOfType('function');
-			var fromValues = list_utils.Iterable.fromValues.bind(list_utils.Iterable);
+			let Iterable = listUtils.Iterable;
+			expect(Iterable.fromValues).toBeOfType('function');
+			var fromValues = Iterable.fromValues.bind(Iterable);
 			expectList(fromValues(), []);
 			expectList(fromValues(true), [true]);
 			expectList(fromValues(1, 2), [1, 2]);
@@ -39,8 +32,9 @@
 		});
 
 		it("`Iterable.toArray` function", function () {
-			expect(list_utils.Iterable.prototype.toArray).toBeOfType('function');
-			var fromValues = list_utils.Iterable.fromValues.bind(list_utils.Iterable);
+			let Iterable = listUtils.Iterable;
+			expect(Iterable.prototype.toArray).toBeOfType('function');
+			var fromValues = Iterable.fromValues.bind(Iterable);
 			expect(fromValues().toArray()).toEqual([]);
 			expect(fromValues(true).toArray()).toEqual([true]);
 			expect(fromValues(1, 2).toArray()).toEqual([1, 2]);
