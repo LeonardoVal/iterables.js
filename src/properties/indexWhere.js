1d046929e2f3d6f78abbabbc3f37e354c17f5c36
Leonardo Val <leonardo.val@creatartis.com>
2019-07-15 00:24:20 -0300

    Removed deprecated code

--- src/properties/indexWhere.js
+++
@@ -1,6 +0,0 @@
-/** `indexWhere(condition, from=0)` returns the position of the first value of this iterable that 
- * complies with the given `condition`, or -1 if there is none.
- */
-Iterable.prototype.indexWhere = function indexWhere(condition, from) {
-	return this.indicesWhere(condition, from).head(-1);
-};
