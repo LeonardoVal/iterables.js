aeb2a831ec5d781c2f7570c8a9ac6e648629ba68
Leonardo Val <leonardo.val@creatartis.com>
2019-07-15 12:07:24 -0300

    Removed deprecated code

--- src/base/lastValue.js
+++
@@ -1,6 +0,0 @@
-/**
- */
-Iterable.prototype.lastValue = function lastValue(defaultValue) {
-	return arguments.length < 1 ? lastFromIterator(__iter__(this)) :
-		lastFromIterator(__iter__(this), defaultValue);
-};
