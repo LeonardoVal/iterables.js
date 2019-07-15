a21beaee8b6b74bd8da5dbf7ca18b0c50cd32b27
Leonardo Val <leonardo.val@creatartis.com>
2019-07-15 00:17:14 -0300

    Enabled tests for empty iterables

--- tests/specs/empty.test.js
+++ tests/specs/empty.test.js
@@ -1,22 +1,11 @@
-﻿define(['list-utils', 'tests-common'], function (list_utils, test_common) { "use strict";
-	var expectIterator = test_common.expectIterator,
-		expectList = test_common.expectList;
+﻿define(['list-utils', 'tests-common'], function (listUtils, test_common) { "use strict";
+	var expectList = test_common.expectList;
 
 	describe("Empty lists:", function () {
-		xit("`Iterable.emptyIterator` function", function () {
-			expect(list_utils.Iterable.emptyIterator).toBeOfType('function');
-			expectIterator(list_utils.Iterable.emptyIterator(), []);
-		});
-
-		xit("`EmptyIterable` class", function () {
-			expect(list_utils.EmptyIterable).toBeOfType('function');
-			expect(list_utils.EmptyIterable.prototype).toBeOfType(list_utils.Iterable);
-			expectList(new list_utils.EmptyIterable(), []);
-		});
-
-		xit("`Iterable.EMPTY` singleton", function () {
-			expect(list_utils.Iterable.EMPTY).toBeOfType(list_utils.EmptyIterable);
-			expectList(list_utils.Iterable.EMPTY, []);
+		it("`EmptyIterable` class", function () {
+			expect(listUtils.EmptyIterable).toBeOfType('function');
+			expect(listUtils.EmptyIterable.prototype).toBeOfType(listUtils.Iterable);
+			expectList(new listUtils.EmptyIterable(), []);
 		});
 	}); // describe "Empty lists:"
 }); //// define
