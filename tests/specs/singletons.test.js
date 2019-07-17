7f82231725c79e19af86eb2b2ea0d9a3c6514401
Leonardo Val <leonardo.val@creatartis.com>
2019-07-17 08:04:23 -0300

    Singleton iterables tests

--- tests/specs/singletons.test.js
+++ tests/specs/singletons.test.js
@@ -1,19 +1,12 @@
-﻿define(['list-utils', 'tests-common'], function (list_utils, test_common) { "use strict";
+﻿define(['list-utils', 'tests-common'], function (listUtils, test_common) { "use strict";
 	var expectIterator = test_common.expectIterator,
 		expectList = test_common.expectList;
 
 	describe("Singletons lists:", function () {
-		xit("`Iterable.singletonIterator` function", function () {
-			expect(list_utils.Iterable.singletonIterator).toBeOfType('function');
-			expectIterator(list_utils.Iterable.singletonIterator(Math.PI), [Math.PI]);
-			expectIterator(list_utils.Iterable.singletonIterator(), [undefined]);
-		});
-
-		xit("`Iterable.singleton` function", function () {
-			expect(list_utils.Iterable.singleton).toBeOfType('function');
-			expect(list_utils.Iterable.singleton()).toBeOfType(list_utils.Iterable);
-			expectList(list_utils.Iterable.singleton(Math.PI), [Math.PI]);
-			expectList(list_utils.Iterable.singleton(), [undefined]);
+		it("`Iterable.singleton` function", function () {
+			expect(listUtils.SingletonIterable).toBeOfType('function');
+			expectList(new listUtils.SingletonIterable(Math.PI), [Math.PI]);
+			expectList(new listUtils.SingletonIterable(), [undefined]);
 		});
 	}); // describe "Singletons lists:"
 }); //// define
