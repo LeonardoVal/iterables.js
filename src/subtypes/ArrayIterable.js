8077da026f15aacc44041540af03bc2c2de52977
Leonardo Val <leonardo.val@creatartis.com>
2019-07-19 12:13:33 -0300

    ArrayIterable can represent an array's slice + Added constructor arguments `__indexFrom__` and `__indexTo__`. + This values constraint the iterations to the given index range. + This allows many slices of the same array to share a reference to the same array.

--- src/subtypes/ArrayIterable.js
+++ src/subtypes/ArrayIterable.js
@@ -1,42 +1,57 @@
 /**
  */
 class ArrayIterable extends Iterable {
-	constructor (array) {
+	constructor (array, indexFrom = 0, indexTo = Infinity) {
 		if (!Array.isArray(array)) {
 			throw new TypeError('Argument must be an array, but is a `'+ typeof array +'`!');
 		}
 		super(array);
+		this.__indexFrom__ = isNaN(indexFrom) ? 0 : Math.max(0, +indexFrom);
+		this.__indexTo__ = isNaN(indexTo) ? Infinity : Math.max(0, +indexTo);
 	}
 
 	[Symbol.iterator]() {
-		let iter = this.source[Symbol.iterator]();
+		let iter;
+		if (this.__indexFrom__ === 0 && this.__indexTo__ >= this.source.length) {
+			iter = this.source[Symbol.iterator]();
+		} else {
+			iter = (function *(array, indexFrom, indexTo) {
+				for (let i = indexFrom; i < indexTo; i++) {
+					yield array[i];
+				}
+			})(this.source, this.__indexFrom__, this.__indexTo__);
+		}
 		return Iterable.__iter__(iter);
 	}
 
 // Conversions /////////////////////////////////////////////////////////////////
 
 	/** @inheritdoc */
-	toSet(set = null) {
-		return set ? super.toSet(set) : new Set(this.source);
+	toArray() {
+		return this.source.slice(this.__indexFrom__, this.__indexTo__);
 	}
 
 // Properties //////////////////////////////////////////////////////////////////
 
 	/** @inheritdoc */
 	isEmpty() {
-		return this.source.length < 1;
+		return this.length < 1;
 	}
 
 	/** @inheritdoc */
 	get length() {
-		return this.source.length;
+		return Math.min(this.source.length, this.__indexTo__) - this.__indexFrom__;
 	}
 
 // Reductions //////////////////////////////////////////////////////////////////
 
 	/** @inheritdoc */
 	reduce(foldFunction, initial) {
-		return this.source.reduce(foldFunction, initial);
+		if (this.__indexFrom__ === 0 && this.__indexTo__ >= this.source.length) {
+			return this.source.reduce(foldFunction, initial);
+		} else {
+			return super.reduce(foldFunction, initial);
+		}
 	}
 
 // Selections //////////////////////////////////////////////////////////////////
@@ -47,7 +62,7 @@ class ArrayIterable extends Iterable {
 		if (!found && arguments.length < 2) {
 			throw new Error(`Cannot get value at ${index}!`);
 		}
-		return found ? this.source[index] : defaultValue;
+		return found ? this.source[index + this.__indexFrom__] : defaultValue;
 	}
 
 } // class ArrayIterable
