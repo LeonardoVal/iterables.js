0ccb6d8b0375c2eebf10f88d3efe5194dc9e125e
Leonardo Val <leonardo.val@creatartis.com>
2019-07-19 12:59:09 -0300

    StringIterable can represent a string's slice + Added constructor arguments `__indexFrom__` and `__indexTo__`. + This values constraint the iterations to the given index range. + This allows many slices of the same string to share a reference to said string.

--- src/subtypes/StringIterable.js
+++ src/subtypes/StringIterable.js
@@ -1,36 +1,42 @@
 /**
  */
 class StringIterable extends Iterable {
-	constructor (string) {
+	constructor (string, indexFrom = 0, indexTo = Infinity) {
 		if (typeof string !== 'string') {
 			throw new TypeError('Argument must be a string, but is a `'+ 
 				typeof string +'`!');
 		}
 		super(string);
+		this.__indexFrom__ = isNaN(indexFrom) ? 0 : Math.max(0, +indexFrom);
+		this.__indexTo__ = isNaN(indexTo) ? Infinity : Math.max(0, +indexTo);
 	}
 
 	[Symbol.iterator]() {
-		let iter = this.source[Symbol.iterator]();
+		let iter;
+		if (this.__indexFrom__ === 0 && this.__indexTo__ >= this.source.length) {
+			iter = this.source[Symbol.iterator]();
+		} else {
+			iter = (function *(string, indexFrom, indexTo) {
+				for (let i = indexFrom; i < indexTo; i++) {
+					yield string.charAt(i);
+				}
+			})(this.source, this.__indexFrom__, this.__indexTo__);
+		}
 		return Iterable.__iter__(iter);
 	}
 
 // Conversions /////////////////////////////////////////////////////////////////
 
-	/** @inheritdoc */
-	toSet(set = null) {
-		return set ? super.toSet(set) : new Set(this.source);
-	}
-
 // Properties //////////////////////////////////////////////////////////////////
 
 	/** @inheritdoc */
 	isEmpty() {
-		return this.source.length < 1;
+		return this.length < 1;
 	}
 
 	/** @inheritdoc */
 	get length() {
-		return this.source.length;
+		return Math.min(this.source.length, this.__indexTo__) - this.__indexFrom__;
 	}
 
 // Selections //////////////////////////////////////////////////////////////////
@@ -41,7 +47,7 @@ class StringIterable extends Iterable {
 		if (!found && arguments.length < 2) {
 			throw new Error(`Cannot get value at ${index}!`);
 		}
-		return found ? this.source.charAt(index) : defaultValue;
+		return found ? this.source.charAt(index + this.__indexFrom__) : defaultValue;
 	}
 
 } // class StringIterable
