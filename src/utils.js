66bfebd212ed48d9adbc589c88aa08a1d36ad559
Leonardo Val <leonardo.val@creatartis.com>
2019-09-23 22:15:22 -0300

    Fix linter errors
    Also added `factorial` function.

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
 /** @ignore */
-export const MAX_INTEGER = Math.pow(2, 53) - 1;
+export const MAX_INTEGER = (2 ** 53) - 1;
 
 /** @ignore */
-export const __toBool__ = (x) => !!x;
+export const toBool = (x) => !!x;
 
 /** @ignore */
 export const toNumber = (x) => +x;
@@ -11,9 +11,22 @@ export const toNumber = (x) => +x;
 export const id = (x) => x;
 
 /** @ignore */
-export const k = (value) => () => value; 
+export const k = (value) => () => value;
 
 /** @ignore */
 export const throwUnimplemented = (methodName, typeName) => {
-	throw new Error(`Function \`${methodName}\` is not implemented for \`${typeName}\`!`);
+  throw new Error(`Function \`${methodName}\` is not implemented for \`${typeName}\`!`);
 };
+
+/** @ignore */
+export function factorial(n, m = 1) {
+  if (Number.isNaN(n) || Number.isNaN(m)) {
+    return NaN;
+  }
+  let r = 1;
+  while (n > m) {
+    r *= n;
+    n -= 1;
+  }
+  return r;
+}
