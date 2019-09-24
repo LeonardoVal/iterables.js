f09dcc345625de0029a799a7d99e8fbea11912d7
Leonardo Val <leonardo.val@creatartis.com>
2019-09-23 23:48:25 -0300

    Removed legacy code

--- src/variadic/union.js
+++
@@ -1,9 +0,0 @@
-/** The `union(lists, equality)` methods treats the given `lists` as sets, and calculates the union
- * of all of them. The `equality` function is used to compare values to avoid repeating them. If it
- * is not given, the standard equality operator (`===`) is used. 
- * 
- * Warning! All the elements of the result are stored in memory.
- */
-Iterable.union = function union(lists, equality) {
-	return Iterable.concat(lists).nub(equality);
-};
