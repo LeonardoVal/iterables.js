0fa0d613b9049e06b9788abb7ea1fb7d046fbf6f
Leonardo Val <leonardo.val@creatartis.com>
2019-09-23 22:10:15 -0300

    Fix linter errors

--- src/subtypes/StringIterable.js
+++ src/subtypes/StringIterable.js
@@ -1,68 +1,78 @@
+import { } from '../generators';
 import Iterable from '../Iterable';
 
 /** Class for representing sequences based on a string, as a whole or in part.
- * 
+ *
  * @augments Iterable
  */
 export class StringIterable extends Iterable {
-	/** The constructor takes a string as a source, and optionally the indices
-	 * of a slice of it.
-	 * 
-	 * @param {string} string - The string to be used as a source of the
-	 * 	sequence.
-	 * @param {integer} [indexFrom=0] - The starting index of the slice of the 
-	 * 	string to be used.
-	 * @param {integer} [indexTo=+Infinity] - The ending index of the slice of 
-	 * 	the string to be used.
-	 * @throws {TypeError} Raises an error if the given source is not a string.
-	*/
-	constructor (string, indexFrom = 0, indexTo = Infinity) {
-		if (typeof string !== 'string') {
-			throw new TypeError('Argument must be a string, but is a `'+ 
-				typeof string +'`!');
-		}
-		super(string);
-		this.__indexFrom__ = isNaN(indexFrom) ? 0 : Math.max(0, +indexFrom);
-		this.__indexTo__ = isNaN(indexTo) ? Infinity : Math.max(0, +indexTo);
-	}
+  /** The constructor takes a string as a source, and optionally the indices
+   * of a slice of it.
+   *
+   * @param {string} string - The string to be used as a source of the
+   *   sequence.
+   * @param {integer} [indexFrom=0] - The starting index of the slice of the
+   *   string to be used.
+   * @param {integer} [indexTo=+Infinity] - The ending index of the slice of
+   *   the string to be used.
+   * @throws {TypeError} Raises an error if the given source is not a string.
+  */
+  constructor(string, indexFrom = 0, indexTo = Infinity) {
+    if (typeof string !== 'string') {
+      throw new TypeError(`Argument must be a string, but is a \`${typeof string}\`!`);
+    }
+    super(string);
+    this.__indexFrom__ = Number.isNaN(indexFrom) ? 0 : Math.max(0, +indexFrom);
+    this.__indexTo__ = Number.isNaN(indexTo) ? Infinity : Math.max(0, +indexTo);
+  }
 
-	/** @inheritdoc */
-	[Symbol.iterator]() {
-		let iter;
-		if (this.__indexFrom__ === 0 && this.__indexTo__ >= this.source.length) {
-			iter = this.source[Symbol.iterator]();
-		} else {
-			iter = (function *(string, indexFrom, indexTo) {
-				for (let i = indexFrom; i < indexTo; i++) {
-					yield string.charAt(i);
-				}
-			})(this.source, this.__indexFrom__, this.__indexTo__);
-		}
-		return Iterable.__iter__(iter);
-	}
+  /** @inheritdoc */
+  [Symbol.iterator]() {
+    let iter;
+    if (this.__indexFrom__ === 0 && this.__indexTo__ >= this.source.length) {
+      iter = this.source[Symbol.iterator]();
+    } else {
+      const generator = function* generator(string, indexFrom, indexTo) {
+        for (let i = indexFrom; i < indexTo; i += 1) {
+          yield string.charAt(i);
+        }
+      };
+      iter = generator(this.source, this.__indexFrom__, this.__indexTo__);
+    }
+    return Iterable.__iter__(iter);
+  }
 
-// Properties //////////////////////////////////////////////////////////////////
+  // Properties //////////////////////////////////////////////////////////////////
 
-	/** @inheritdoc */
-	isEmpty() {
-		return this.length === 0;
-	}
+  /** @inheritdoc */
+  isEmpty() {
+    return this.length === 0;
+  }
 
-	/** @inheritdoc */
-	get length() {
-		return Math.max(0, Math.min(this.source.length, this.__indexTo__) -
-			this.__indexFrom__);
-	}
+  /** @inheritdoc */
+  get length() {
+    return Math.max(0,
+      Math.min(this.source.length, this.__indexTo__) - this.__indexFrom__);
+  }
 
-// Selections //////////////////////////////////////////////////////////////////
+  // Selections //////////////////////////////////////////////////////////////////
 
-	/** @inheritdoc */
-	get(index, defaultValue) {
-		let found = index >= 0 && index < this.length;
-		if (!found && arguments.length < 2) {
-			throw new Error(`Cannot get value at ${index}!`);
-		}
-		return found ? this.source.charAt(index + this.__indexFrom__) : defaultValue;
-	}
-
+  /** @inheritdoc */
+  get(index, defaultValue) {
+    const found = index >= 0 && index < this.length;
+    if (!found && arguments.length < 2) {
+      throw new Error(`Cannot get value at ${index}!`);
+    }
+    return found ? this.source.charAt(index + this.__indexFrom__) : defaultValue;
+  }
 } // class StringIterable
+
+/** Makes an `Iterable` for the given `string`. If the argument is not a
+ * string, it is converted to one.
+ *
+ * @param {any} string - The string to be used as a source.
+ * @returns {StringIterable<string>}
+ */
+export function fromString(string) {
+  return new StringIterable(`${string}`);
+}
