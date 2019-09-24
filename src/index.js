265aa452f5617275deb66df2101bc3c2f30888c7
Leonardo Val <leonardo.val@creatartis.com>
2019-09-23 22:13:14 -0300

    Set exports after deleted submodule

--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@ import asyncGenerators from './generators-async';
 import AbstractIterable from './AbstractIterable';
 import Iterable from './Iterable';
 import AsyncIterable from './AsyncIterable';
-import subtypes from './subtypes';
+// subtypes
+import { ArrayIterable, fromArray, fromValues } from './subtypes/ArrayIterable';
+import { EmptyIterable, EMPTY } from './subtypes/EmptyIterable';
+import EnumerationIterable from './subtypes/EnumerationIterable';
+import MapIterable from './subtypes/MapIterable';
+import { ObjectIterable, fromObject } from './subtypes/ObjectIterable';
+import SetIterable from './subtypes/SetIterable';
+import SingletonIterable from './subtypes/SingletonIterable';
+import { StringIterable, fromString } from './subtypes/StringIterable';
 
 export default {
-	AbstractIterable,
-	Iterable,
-	AsyncIterable,
-	subtypes,
+  AbstractIterable,
+  Iterable,
+  AsyncIterable,
+  ArrayIterable,
+  fromArray,
+  fromValues,
+  EmptyIterable,
+  EMPTY,
+  EnumerationIterable,
+  MapIterable,
+  ObjectIterable,
+  fromObject,
+  SetIterable,
+  SingletonIterable,
+  StringIterable,
+  fromString,
   generators: { ...generators, async: asyncGenerators },
-}
+};
