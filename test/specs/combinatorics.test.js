1e941a59457dacf49209fef7b1e8dfe64985818d
Leonardo Val <leonardo.val@creatartis.com>
2019-10-05 11:06:33 -0300

    Enable test cases for combinatorics
    Also fixed one case for combinations.

--- test/specs/combinatorics.test.js
+++ test/specs/combinatorics.test.js
@@ -1,10 +1,10 @@
-﻿/* globals describe expect xit */
+﻿/* globals describe expect it */
 /* eslint-disable import/no-unresolved */
 import { fromArray, Iterable } from '../../src/index';
 import { expectList } from './tests-common';
 
 describe('Combinatorics:', () => {
-  xit('`Iterable.permutations` function', () => {
+  it('`Iterable.permutations` function', () => {
     expect(Iterable.prototype.permutations).toBeOfType('function');
     expectList(fromArray([0, 1, 2]).permutations(0), []);
     expectList(fromArray([0, 1, 2]).permutations(1), [[0], [1], [2]]);
@@ -15,9 +15,9 @@ describe('Combinatorics:', () => {
     expectList(fromArray([0, 1, 2]).permutations(4), []);
   });
 
-  xit('`Iterable.combinations` function', () => {
+  it('`Iterable.combinations` function', () => {
     expect(Iterable.prototype.combinations).toBeOfType('function');
-    expectList(fromArray([0, 1, 2]).combinations(0), []);
+    expectList(fromArray([0, 1, 2]).combinations(0), [[]]);
     expectList(fromArray([0, 1, 2]).combinations(1), [[0], [1], [2]]);
     expectList(fromArray([0, 1, 2]).combinations(2), [[0, 1], [0, 2], [1, 2]]);
     expectList(fromArray([0, 1, 2]).combinations(3), [[0, 1, 2]]);
